c3f8b0265687f9d6cbe7223e9c4e42cbe529a5bc
binarylover <whitedevil.hamd@gmail.com>
2024-03-05 16:48:31 +0530

    "c ++ program added"

---
+++ C++ practicals/practical3.cpp
@@ -0,0 +1,22 @@
+// frequency of each character in a string 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string str = "Hello world!";
+  int freq[256] = {0};
+
+  for (char c : str) {
+    freq[c]++;
+  }
+
+  for (int i = 0; i < 256; i++) {
+    if (freq[i] > 0) {
+      cout << (char)i << ": " << freq[i] << endl;
+    }
+  }
+
+  return 0;
+}
