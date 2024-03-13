e1379d6cea1458aa03b968ac9d8a36256c00c412
binarylover <whitedevil.hamd@gmail.com>
2024-03-13 18:05:56 +0530

    practical done

--- C++ practicals/practical3.cpp
+++ C++ practicals/practical3.cpp
@@ -4,16 +4,19 @@
 using namespace std;
 
 int main() {
-  string str = "Hello world!";
+  string str;
+  cout << "Enter a string: ";
+  getline(cin, str); 
   int freq[256] = {0};
 
   for (char c : str) {
     freq[c]++;
   }
 
-  for (int i = 0; i < 256; i++) {
-    if (freq[i] > 0) {
-      cout << (char)i << ": " << freq[i] << endl;
+  for (int i = 0; i < str.length();i++){
+    if (freq[str[i]] != 0){
+      cout << str[i] << ": " << freq[str[i]] << endl;
+      freq[str[i]] = 0;
     }
   }
 
